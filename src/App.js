2964b7c6e3c3fb18805d6b8137c9dc00c22b243e
MaxenceAll <maxence.allart@gmail.com>
2023-03-05 21:14:20 +0100

    vansdetails

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import "./App.css";
 
+import './server'
+import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
+
 function App() {
   return (
     <BrowserRouter>
       <header>
-        <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
+        <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
-          <Link to="/about">About!!</Link>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
