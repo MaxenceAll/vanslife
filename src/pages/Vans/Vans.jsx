4603fed727a57d6f202420a90f71d678ac2dd96c
MaxenceAll <maxence.allart@gmail.com>
2023-03-06 22:20:08 +0100

    last

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,8 +1,13 @@
+import { type } from "@testing-library/user-event/dist/type";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 function Vans() {
   const [vans, setVans] = useState([]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.get("type");
+  console.log(typeFilter);
 
   useEffect(() => {
     fetch("/api/vans")
@@ -10,9 +15,14 @@ function Vans() {
       .then((data) => setVans(data.vans));
   }, []);
 
-  console.log(vans);
+  // console.log(vans);
+  const filteredVans = typeFilter
+    ? vans.filter((van) => van.type === typeFilter)
+    : vans;
 
-  const vanElements = vans.map((van) => {
+  console.log(filteredVans);
+
+  const vanElements = filteredVans.map((van) => {
     return (
       <Link to={`/vans/${van.id}`}>
         <div key={van.id} className="van-tile">
@@ -33,9 +43,52 @@ function Vans() {
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
-      <div className="van-list">
-        {vanElements}
+      <div className="van-list-filter-buttons">
+
+
+          {/* <Link 
+          to="?type=simple"
+          className="van-type simple"
+          >Simple</Link>
+          
+          <Link 
+          to="?type=rugged"
+          className="van-type rugged"
+          >Rugged</Link>
+
+          <Link 
+          to="?type=luxury"
+          className="van-type luxury"
+          >Luxury</Link>
+
+          <Link 
+          to="."
+          className="van-type clear-filters"
+          >Clear</Link> */}
+
+          <button 
+          onClick={()=> setSearchParams({type: "simple"})}
+          className="van-type simple"
+          >Simple</button>
+
+          <button 
+          onClick={()=> setSearchParams({type: "rugged"})}
+          className="van-type rugged"
+          >Rugged</button>
+
+          <button 
+          onClick={()=> setSearchParams({type: "luxury"})}
+          className="van-type luxury"
+          >Luxury</button>
+
+          <button 
+          onClick={()=> setSearchParams({})}
+          className="van-type clear-filters"
+          >Clear</button>
+
+
       </div>
+      <div className="van-list">{vanElements}</div>
     </div>
   );
 }
